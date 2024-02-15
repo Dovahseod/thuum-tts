cba1b8c61912b5cc801a4770e428bfd906c6cf3b
Dovahseod <systemme@outlook.com>
2024-02-15 16:26:27 +0800

    Primary key and type definitions for the database

--- src/utils/init_database.py
+++ src/utils/init_database.py
@@ -2,6 +2,16 @@ import aiosqlite
 
 
 async def initialize_database(db: aiosqlite.Connection):
-    cursor = await db.execute("CREATE TABLE preferences(guild, user, voice, wpm, wordgap, pitch, amplitude)")
+    cursor = await db.execute(
+        """CREATE TABLE preferences(
+                guild INTEGER NOT NULL,
+                user INTEGER NOT NULL,
+                voice TEXT,
+                wpm INTEGER,
+                wordgap INTEGER,
+                pitch INTEGER,
+                amplitude INTEGER
+                PRIMARY KEY(guild, user)
+            )""")
     await db.commit()
     await cursor.close()
