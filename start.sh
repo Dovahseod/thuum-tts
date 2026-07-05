f6518536bbef369c2ab1e4827f875ff96241da60
Dovahseod <101095665+Dovahseod@users.noreply.github.com>
2026-07-05 23:00:46 +0800

    better start script

--- start.sh
+++ start.sh
@@ -1,4 +1,15 @@
-until python3 src/main.py; do
-	echo "Thu'um TTS bot crashed with exit code $?. Restarting..." >&2
-	sleep 1
+#!/bin/bash
+
+while true; do
+    python3 src/main.py
+    EXIT_CODE=$?
+
+    # Restart if exit code is not 0 (normal termination) or 130 (KeyboardInterrupt)
+    if [ $EXIT_CODE -eq 0 ] || [ $EXIT_CODE -eq 130 ]; then
+        echo "Stopping with exit code $EXIT_CODE."
+        break
+    else
+        echo "Thu'um TTS bot crashed with exit code $EXIT_CODE. Restarting..." >&2
+        sleep 1
+    fi
 done
